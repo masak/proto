690dcd875bd571b1a3a438dbd850b96ae86b4150
carlin <carlin-public@theintersect.org>
2009-09-29 00:45:20 +1300

    [tab-completion.sh] Tab-complete the project name for every parameter after the initial command, rather than only on the second parameter

--- tab-completion.sh
+++ tab-completion.sh
@@ -21,8 +21,10 @@ _proto ()
 		return 0
 	fi
 
-	if [[ $COMP_CWORD -eq 2 ]]; then
-		COMPREPLY=( $( compgen -W '$( command cat projects.list | grep -i '^[a-z0-9\-]*:$' | sed -e 's/://' )' -- $cur ) )
+	if [[ $COMP_CWORD > 1 ]]; then
+		COMPREPLY=( $( compgen -W '$( command cat projects.list \
+			| grep -i '^[a-z0-9\-]*:$' \
+			| sed -e 's/://' )' -- $cur ) )
 		return 0
 	fi
 }
