ae47203d694bcf1fba81c8756aab08bb63f15220
Zoffix Znet <cpan@zoffix.com>
2015-10-13 17:44:19 -0400

    Handle cases where user presses "Back" button (Closes #15)

--- web/js/main.js
+++ web/js/main.js
@@ -1,2 +1,22 @@
 jQuery(function ($) {
+    setup_search_box();
 });
+
+function setup_search_box() {
+    var el = $('#my_search_box .search');
+    if ( ! el.length ) { return; }
+
+    // Here we have logic for handling cases where user views a repo
+    // and then presses "Back" to go back to the list. We want to
+    //     (a) NOT scroll to search input if scroll position is offset.
+    //          This is a "Back" after scrolling and viewing a repo
+    //     (b) Refresh 'search' if search input has some text.
+    //          This is a "Back" after clicking on search results
+    if ( $(window).scrollTop() == 0 ) {
+        el.focus();
+    }
+
+    if ( el.val().length ) {
+        userList.search(el.val());
+    }
+}
