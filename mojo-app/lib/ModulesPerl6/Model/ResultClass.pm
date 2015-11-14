7190eec7dc9113c934d4582df0610491a1955e82
Zoffix Znet <cpan@zoffix.com>
2015-11-14 08:58:35 -0500

    Docs for ModulesPerl6::Model::ResultClass

--- mojo-app/lib/ModulesPerl6/Model/ResultClass.pm
+++ mojo-app/lib/ModulesPerl6/Model/ResultClass.pm
@@ -7,3 +7,40 @@ sub import {
 }
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+ModulesPerl6::Model::ResultClass - import base for ::Result classes
+
+=head1 SYNOPSIS
+
+    package ModulesPerl6::Model::Foo::Schema::Result::Bars;
+    use     ModulesPerl6::Model::ResultClass;
+
+    primary_column some_id => { data_type => 'text' };
+
+    1;
+
+=head1 DESCRIPTION
+
+Simply C<use> this module in your L<DBIx::Class>
+L<https://metacpan.org/pod/DBIx::Class::ResultSource|Result classes>
+to import L<strictures> and L<DBIx::Class::Candy>. i.e. it's the same
+as doing this in your class, except should that boilerplate change, you'd
+only have to change it in class and not each Result class:
+
+    use strictures;
+    use DBIx::Class::Candy;
+
+=head1 CONTACT INFORMATION
+
+Original version of this module was written by Zoffix Znet
+(L<https://github.com/zoffixznet/>, C<Zoffix> on irc.freenode.net).
+
+=head1 LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
