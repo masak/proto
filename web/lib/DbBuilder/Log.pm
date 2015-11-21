d87f592c3d65801f7fe6bb06ab96e68befdf167b
Zoffix Znet <cpan@zoffix.com>
2015-11-21 12:15:42 -0500

    Added DbBuilder::Log and its docs/tests

---
+++ web/lib/DbBuilder/Log.pm
@@ -0,0 +1,62 @@
+package DbBuilder::Log;
+
+use Exporter::Easy EXPORT => [ 'log' ];
+use Mojo::Log;
+
+my $LOG = Mojo::Log->new;
+use 5.020;
+sub log($$) {
+    my ( $level, $message ) = @_;
+    $LOG->$level( $message );
+    $level eq 'fatal' and die "*died through fatal level log message*\n";
+
+    return $message;
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+DbBuilder::Log - convenient logging
+
+=head1 SYNOPSIS
+
+    use DbBuilder::Log;
+
+    log info  => 'Starting stuff';
+    log fatal => 'Oh noes!'; # dies after logging
+
+    OUTPUT:
+    [Sat Nov 21 12:12:30 2015] [info] Starting stuff
+    [Sat Nov 21 12:12:30 2015] [fatal] Oh noes!
+    *died through fatal level log message*
+
+=head1 DESCRIPTION
+
+This module is used to access and manipulate the database of Perl 6
+distributions that is built by the build script.
+
+=head1 EXPORTED SUBROUTINES
+
+=head2 C<log>
+
+    log info  => 'Starting stuff';
+    log fatal => 'Oh noes!'; # dies after logging
+
+B<Takes> log level and the log message and prints those along
+with a time stamp. Valid log levels are C<debug>, C<info>, C<warn>,
+C<error>, and c<fatal>. If C<fatal> log level is used, the subroutine will
+C<die> after printing the error message.
+
+=head1 CONTACT INFORMATION
+
+Original version of this module was written by Zoffix Znet
+(L<https://github.com/zoffixznet/>, C<Zoffix> on irc.freenode.net).
+
+=head1 LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
---
+++ web/t/02-db-builder/01-logger.t
@@ -0,0 +1,33 @@
+#!perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use Test::Most;
+use Test::Output qw/combined_from/;
+
+BEGIN { use_ok 'DbBuilder::Log' };
+
+my ( $out, $ret );
+my $time_stamp_re = qr/\[\w{3} \w{3} \d\d? \d{2}:\d{2}:\d{2} \d{4}\]/;
+
+for ( qw/debug info warn error/ ) {
+    $out = combined_from sub { $ret = log $_ => "This is $_ log"; };
+    like $out => qr/^$time_stamp_re \[$_\] This is $_ log$/, "$_ log works";
+    is $ret, "This is $_ log", 'return value of log() is the message';
+}
+
+subtest 'fatal level log' => sub {
+    $out = combined_from sub { eval { log fatal => 'This is fatal log' } };
+    like $out => qr/^$time_stamp_re \[fatal\] This is fatal log$/,
+            'fatal log has correct message';
+
+    throws_ok { combined_from sub {log fatal  => 'dies';} }
+        qr/^\*died through fatal level log message\*$/, 'fatal log dies';
+};
+
+dies_ok { log foobar => 'dies'; } 'dies on non-existent log level';
+throws_ok { eval "log info => 'foo', 'bar'"; $@ and die; }
+    qr/Too many arguments/, 'prototype catches screw ups';
+
+done_testing;
