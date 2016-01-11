15d1a08d1d3fefddec0d19dd9429923927c8460a
Wenzel P. P. Peppmeyer <wenzel.peppmeyer@gmx.de>
2016-01-11 16:07:57 +0100

    _get_travis_status does test for 'finished' now see: https://api.travis-ci.org/repos/gfldex/perl6-xhtml-writer/builds

--- lib/ModulesPerl6/DbBuilder/Dist/PostProcessor/TravisCI.pm
+++ lib/ModulesPerl6/DbBuilder/Dist/PostProcessor/TravisCI.pm
@@ -52,6 +52,7 @@ sub _get_travis_status {
     return $state    if $state =~ /cancel|pend/;
     return 'error'   if $state =~ /error/;
     return 'failing' if $state =~ /fail/;
+	return 'passing' if $state =~ /finished/;
     return 'passing' if $state =~ /pass/;
     return 'unknown';
 }
