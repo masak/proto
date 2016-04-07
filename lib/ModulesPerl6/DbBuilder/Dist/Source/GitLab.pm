d8ad2a62a5b36fc96f579633f21e24d3f7c52b50
Steve Mynott <steve.mynott@gmail.com>
2016-04-07 16:50:34 +0100

    minimal GitLab support (displays time as start of epoch)

---
+++ lib/ModulesPerl6/DbBuilder/Dist/Source/GitLab.pm
@@ -0,0 +1,60 @@
+package ModulesPerl6::DbBuilder::Dist::Source::GitLab;
+
+use base 'ModulesPerl6::DbBuilder::Dist::Source';
+
+use Carp       qw/croak/;
+use Mojo::Util qw/slurp  decode/;
+
+use ModulesPerl6::DbBuilder::Log;
+use Mew;
+use experimental 'postderef';
+
+sub re {
+    qr{
+        ^   https?:// \Qgitlab.com\E
+            /([^/]+)    # User
+            /([^/]+)    # Repo
+        $
+    }ix;
+}
+
+sub load {
+    my $self = shift;
+
+    log info => 'Fetching distro info and commits';
+    my $dist    = $self->_dist or return;
+
+    $dist->{author_id} = $dist->{_builder}{repo_user}
+        if $dist->{author_id} eq 'N/A';
+
+    $dist->{date_updated}=undef; # XXX displays as Epoch start in UI
+
+    return if $dist->{name} eq 'N/A';
+
+    return $dist;
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=for stopwords md dist dists
+
+=head1 NAME
+
+ModulesPerl6::DbBuilder::Dist::Source::GitLab - GitLab distribution source
+
+=head1 DOCUMENTATION
+
+See documentation for L<ModulesPerl6::DbBuilder::Dist::Source> for details.
+
+=head1 CONTACT INFORMATION
+
+Original version of this module was written by Zoffix Znet
+(L<https://github.com/zoffixznet/>, C<Zoffix> on irc.freenode.net).
+
+=head1 LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
