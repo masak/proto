424adb1157a7640e5cfbe7dd0bb2236d811e6b0b
Zoffix Znet <cpan@zoffix.com>
2017-03-10 19:40:12 -0500

    Ensure we remove any unused tags from DB

--- lib/ModulesPerl6/Model/Dists/Schema/Result/TagDist.pm
+++ lib/ModulesPerl6/Model/Dists/Schema/Result/TagDist.pm
@@ -1,8 +1,8 @@
 package ModulesPerl6::Model::Dists::Schema::Result::TagDist;
 use     ModulesPerl6::Model::ResultClass;
 
-primary_column tag  => { data_type => 'text' };
-primary_column dist => { data_type => 'text' };
+primary_column tag  => { data_type => 'text', is_foreign_key => 1  };
+primary_column dist => { data_type => 'text', is_foreign_key => 1  };
 
 belongs_to tag  => 'ModulesPerl6::Model::Dists::Schema::Result::Tag';
 belongs_to dist => 'ModulesPerl6::Model::Dists::Schema::Result::Dist';
@@ -10,4 +10,3 @@ belongs_to dist => 'ModulesPerl6::Model::Dists::Schema::Result::Dist';
 1;
 
 __END__
-
