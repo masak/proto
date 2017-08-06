f9edddfc42819fe4e79fd591f314325c6d89b680
Zoffix Znet <cpan@zoffix.com>
2017-08-06 02:31:37 -0400

    Toss largely wrong doc

--- lib/ModulesPerl6/Model/Dists/Schema/Result/AppveyorStatus.pm
+++ lib/ModulesPerl6/Model/Dists/Schema/Result/AppveyorStatus.pm
@@ -10,24 +10,3 @@ has_many dists
 1;
 
 __END__
-
-=encoding utf8
-
-=for stopwords dists
-
-=head1 NAME
-
-ModulesPerl6::Model::Dists::Schema::Result::AppveyorStatus - Appveyor statuses
-
-=head1 DESCRIPTION
-
-This table stores L<https://appveyor.com/> statuses for the dists.
-
-=head1 CONTACT INFORMATION
-
-Original version of this module was written by Zoffix Znet
-(L<https://github.com/zoffixznet/>, C<Zoffix> on irc.freenode.net).
-
-=head1 LICENSE
-
-You can use and distribute this module under the same terms as Perl itself.
