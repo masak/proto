46bfeea222f9ec6261241e350ebeef6c2184a985
Zoffix Znet <cpan@zoffix.com>
2017-08-07 23:49:13 -0400

    Include core modules in the listing
    Since it may not be obvious where to look for their documentation to
    users unfamiliar with our site. Make them link to docs for the topic.
    
    The Feelin'-Lucky search will direct to them on exact name match,
    otherwise, it'll search through them only when no other dists were found.

--- lib/ModulesPerl6/Controller/Root.pm
+++ lib/ModulesPerl6/Controller/Root.pm
@@ -3,6 +3,7 @@ package ModulesPerl6::Controller::Root;
 use Mojo::Base 'Mojolicious::Controller';
 
 use Mojo::URL;
+use ModulesPerl6::Model::CoreModules;
 use List::UtilsBy qw/nsort_by  uniq_by/;
 use POSIX qw/strftime/;
 use experimental 'postderef';
@@ -32,6 +33,7 @@ sub search {
     return $self->lucky if $self->param('lucky');
 
     my @dists;
+    my $core_dists;
     if (length (my $q = $self->param('q'))) {
         (my $opts, $q) = $self->_parse_search_options($q);
 
@@ -49,9 +51,12 @@ sub search {
 
         @dists = grep lc($_->{dist_source}) eq $opts->{dist_source}, @dists,
             if $opts->{dist_source};
+
+        $core_dists = ModulesPerl6::Model::CoreModules->new->find($q);
     }
     else {
         @dists = $self->dists->find->each;
+        $core_dists = ModulesPerl6::Model::CoreModules->new->all;
     }
 
     @dists = nsort_by { -$_->{stars} } @dists;
@@ -77,12 +82,16 @@ sub search {
         ),
         tags  => $tags->{by_count},
         dists => \@dists,
+        core_dists => $core_dists,
         body_class => 'page_search',
     );
 
     $self->respond_to(
         html => { %data, template => 'root/search' },
-        json => { json => { dists => \@dists } },
+        json => { json => {
+            dists      => \@dists,
+            core_dists => $core_dists->to_array,
+        } },
     );
 }
 
@@ -90,9 +99,19 @@ sub lucky {
     my $self = shift;
 
     my $q = $self->param('q') // '';
+
+    if (my ($mod) = ModulesPerl6::Model::CoreModules->new->named($q)->@*) {
+        return $self->redirect_to($mod->{url})
+    }
+
     my @dists = $self->dists->find({ name        => \$q })->each,
                 $self->dists->find({ description => \$q })->each;
-    @dists or return $self->reply->not_found;
+
+    unless (@dists) {
+        my ($mod) = ModulesPerl6::Model::CoreModules->new->find($q)->@*;
+        $mod ? return $self->redirect_to($mod->{url})
+             : return $self->reply->not_found;
+    }
 
     my $dist = (grep $_->{name} eq $q, @dists)[0]
          || (nsort_by { -$_->{stars} } @dists)[0];
---
+++ lib/ModulesPerl6/Model/CoreModules.pm
@@ -0,0 +1,46 @@
+package ModulesPerl6::Model::CoreModules;
+
+use Mew;
+use Mojo::Collection qw/c/;
+use Mojo::Util qw/trim/;
+
+use constant MODULES => c
+    {
+        name        => 'Test',
+        description => 'Routines for testing your code',
+        url         => 'https://docs.perl6.org/language/testing',
+    },
+    {
+        name        => 'NativeCall',
+        description => 'Native calling interface for using C libraries',
+        url         => 'https://docs.perl6.org/language/nativecall',
+    },
+    {
+        name        => 'Pod::To::Text',
+        description => 'Render POD as Text',
+        url         => 'https://docs.perl6.org/language/pod#Text',
+    };
+
+sub __clone_and_core {
+    shift->each(sub { +{%$_, is_core => 1} })
+}
+
+sub all { __clone_and_core MODULES }
+
+sub named {
+    my ($self, $name) = @_;
+    __clone_and_core MODULES->grep(sub {
+        CORE::fc($_->{name}) eq CORE::fc($name)
+    })
+}
+
+sub find {
+    my ($self, $q) = @_;
+    __clone_and_core MODULES->grep(sub {
+        "$_->{name} $_->{description}" =~ /\Q$q\E/i
+    })
+}
+
+1;
+
+__END__
