a13c7f81c886d171a19dd7381a85103dd7c265d3
Zoffix Znet <cpan@zoffix.com>
2017-10-04 10:18:44 -0400

    Shave off 431b per dist in search results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,40 @@
 $(function(){
     setup_tags();
     setup_code_highlights();
+    setup_gen_search_info();
+
     $(document).ready(function(){
         $('[data-toggle="tooltip"]').tooltip();
     });
 });
+
+function setup_gen_search_info() {
+    var el = $('#dist-list,#core-dist-list');
+    if (! el.length) return;
+    el.find('[class^="glyphicon-"]').addClass('glyphicon');
+    el.find('h2 i').each(function() {
+        $(this).addClass('icon-24-' + $(this).attr('title'))
+        .attr('title', 'Hosted on: ' + $(this).attr('title'))
+        .attr('data-toggle', 'tooltip');
+    });
+    el.find('.glyphicon-star').parent('li')
+        .attr('title', 'Number of likes').attr('data-toggle', 'tooltip');
+    el.find('.glyphicon-info-sign').parent('li')
+        .attr('title', 'Number of open Issues').attr('data-toggle', 'tooltip')
+        .attr('class', 'issues');
+    el.find('.travis').each(function() {
+        $(this).attr('title', 'Travis status: ' + $(this).attr('title'))
+        .attr('data-toggle', 'tooltip');
+    });
+    el.find('.appveyor').each(function() {
+        $(this).attr('title', 'Appveyor status: ' + $(this).attr('title'))
+        .attr('data-toggle', 'tooltip');
+    });
+    el.find('.glyphicon-wrench').parent('li')
+        .attr('title', 'Last updated on').attr('data-toggle', 'tooltip');
+    el.find('.btn-group a').addClass('btn btn-xs btn-default');
+    el.find('ul,.btn-group').css({visibility: 'visible'});
+}
 
 function setup_code_highlights() {
     $('.code-mirror').each(function(i,el){
